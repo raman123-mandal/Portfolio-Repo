ffd6a33ef577b167bc4437a53907e5ef0ca09574
ramanjeet mandal <ramanjeetmandal4619@gamil.com>
2026-02-11 11:48:37 +0530

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
 import SkillsPage from './pages/SkillsPage'
